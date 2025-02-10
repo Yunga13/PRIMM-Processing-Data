1051dbb82d80e75f43b1a9f178639cea5bff6a15
Yunga13 <yungayunga13@gmail.com>
2025-02-10 22:45:59 +0000

    ajslkdfajds

--- process_data.py
+++ process_data.py
@@ -9,6 +9,8 @@ name - date
 import csv
 from typing import Union
 
+record = dict[str, Union[str, int, float]]
+
 def get_records(data_filename: str) -> list[dict[str, Union[str, int]]]:
   """
   Gets records from a csv data file.
@@ -27,10 +29,35 @@ def get_records(data_filename: str) -> list[dict[str, Union[str, int]]]:
 
   return records
 
+def convert_fields(records: list[dict[str, Union[str, int]]]) -> None:
+  for record in records:
+    record["REPORTYEAR"] = int(record["REPORTYEAR"])
+    record["PropValue"] = int(record["PropValue"])
+
+def calculate_avg(records, k:str) -> float:
+  total:int = 0
+  for record in records:
+    total += record[k]
+  return total/len(records)
+
+def summarize_by_month(records: list[record]) -> dict[str, int]:
+  summary: dict[str, int] = {}
+  for r in records:
+    month: str = r["REPORTDATE"][5:7]
+    if month in summary:
+      summary[month] += 1
+    else:
+      summary[month] = 1
+  return summary
 def main() -> None:
   data_filename: str = "resources/stolen_bikes.csv"
-  records: list[dict[str, Union[str, int]]] = get_records(data_filename)
+  records: list[record] = get_records(data_filename)
+  convert_fields(records)
 
+  summary: dict[str, int] = summarize_by_month(records)
+  print(f"{summarize_by_month}: {summary[month]}")
+  avg:float = calculate_avg(records, "proval")
+  print(f"Average number of bikes stolen{avg}")
   print(f"{len(records)} records read in.")
   print(records[0])
   print(records[0]["District"])
